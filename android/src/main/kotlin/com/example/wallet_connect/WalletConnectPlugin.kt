61bf49f4de32a85c1457a3bf55f14e2e0414a773
Marius Gill <35896407+Dryec@users.noreply.github.com>
2020-09-30 16:04:04 +0200

    -

--- android/src/main/kotlin/com/example/wallet_connect/WalletConnectPlugin.kt
+++ android/src/main/kotlin/com/example/wallet_connect/WalletConnectPlugin.kt
@@ -1,6 +1,16 @@
 package com.example.wallet_connect
 
 import androidx.annotation.NonNull
+import com.trustwallet.walletconnect.WCClient
+import com.trustwallet.walletconnect.models.WCPeerMeta
+import com.trustwallet.walletconnect.models.WCSignTransaction
+import com.trustwallet.walletconnect.models.binance.WCBinanceCancelOrder
+import com.trustwallet.walletconnect.models.binance.WCBinanceTradeOrder
+import com.trustwallet.walletconnect.models.binance.WCBinanceTransferOrder
+import com.trustwallet.walletconnect.models.binance.WCBinanceTxConfirmParam
+import com.trustwallet.walletconnect.models.ethereum.WCEthereumSignMessage
+import com.trustwallet.walletconnect.models.ethereum.WCEthereumTransaction
+import com.trustwallet.walletconnect.models.session.WCSession
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -8,6 +18,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import okhttp3.*
 
 /** WalletConnectPlugin */
 class WalletConnectPlugin: FlutterPlugin, MethodCallHandler {
@@ -16,15 +27,40 @@ class WalletConnectPlugin: FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
+  private lateinit var client : WCClient
+
+  private var peerMeta = WCPeerMeta("Vision", "https://vision-crypto.com")
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "wallet_connect")
     channel.setMethodCallHandler(this)
+
+    client = WCClient(httpClient = OkHttpClient())
+    client.onFailure = ::onFailure
+    client.onDisconnect = ::onDisconnect
+    client.onSessionRequest = ::onSessionRequest
+    client.onEthSign = ::onEthSign
+    client.onEthSignTransaction = ::onEthSignTransaction
+    client.onEthSendTransaction = ::onEthSendTransaction
+    client.onCustomRequest = ::onCustomRequest
+    client.onBnbTrade = ::onBnbTrade
+    client.onBnbCancel = ::onBnbCancel
+    client.onBnbTransfer = ::onBnbTransfer
+    client.onBnbTxConfirm = ::onBnbTxConfirm
+    client.onGetAccounts = ::onGetAccounts
+    client.onSignTransaction = ::onSignTransaction
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "connectToSessionString") {
+      var session = call.argument<String>("session")?.let { WCSession.from(it) }
+
+      if(session == null) {
+        result.error("missing_session", "No session provided", "null")
+        return
+      }
+
+      client.connect(session, peerMeta)
     } else {
       result.notImplemented()
     }
@@ -33,4 +69,44 @@ class WalletConnectPlugin: FlutterPlugin, MethodCallHandler {
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
   }
+
+  private fun onFailure(throws: Throwable) {
+  }
+
+  private fun onDisconnect(code: Int, reason: String) {
+  }
+
+  private fun onSessionRequest(id: Long, peer: WCPeerMeta) {
+  }
+
+  private fun onEthSign(id: Long, message: WCEthereumSignMessage) {
+  }
+
+  private fun onEthSignTransaction(id: Long, transaction: WCEthereumTransaction) {
+  }
+
+  private fun onEthSendTransaction(id: Long, transaction: WCEthereumTransaction) {
+  }
+
+  private fun onCustomRequest(id: Long, payload: String) {
+  }
+
+  private fun onBnbTrade(id: Long, order: WCBinanceTradeOrder) {
+  }
+
+  private fun onBnbCancel(id: Long, order: WCBinanceCancelOrder) {
+  }
+
+  private fun onBnbTransfer(id: Long, order: WCBinanceTransferOrder) {
+  }
+
+  private fun onBnbTxConfirm(id: Long, order: WCBinanceTxConfirmParam) {
+  }
+
+  private fun onGetAccounts(id: Long) {
+  }
+
+  private fun onSignTransaction(id: Long, transaction: WCSignTransaction) {
+  }
+
 }
--- lib/wallet_connect.dart
+++ lib/wallet_connect.dart
@@ -1,14 +1,14 @@
-
 import 'dart:async';
 
+import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 
 class WalletConnect {
-  static const MethodChannel _channel =
-      const MethodChannel('wallet_connect');
+  static const MethodChannel _channel = const MethodChannel('wallet_connect');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future connect() async {
+    var result = await BarcodeScanner.scan();
+    await _channel
+        .invokeMethod('connectToSessionString', {'session': result.rawContent});
   }
 }
